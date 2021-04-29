a1035a11305500b6e9e74a9bd21f082bb231f3a6
Edwin Dharmawan <edwindharmawan@refactory.id>
2021-04-29 16:20:53 +0700

    add logic/palindrome

--- Logic/palindrome/index.js
+++ Logic/palindrome/index.js
@@ -5,3 +5,19 @@
 // Malas                 # --> not palindrome
 // Makan nasi goreng     # --> not palindrome
 // Balonku ada lima      # --> not palindrome
+
+function checkPalindrome(input) {
+    let data = input.toLowerCase()
+    let result = ""
+    for (let i = data.length - 1; i >= 0; i--) {
+        result += data[i]
+    }
+    if (data === result) {
+        return "palindrome"
+    } else {
+        return "not palindrome"
+    }
+}
+
+
+console.log(checkPalindrome("Balonku ada lima"))
