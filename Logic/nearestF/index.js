cf3f240fa152928d942213d9b37b0ec877e216f5
Edwin Dharmawan <edwindharmawan@refactory.id>
2021-04-29 17:11:27 +0700

    move folder

---
+++ Logic/nearestF/index.js
@@ -0,0 +1,12 @@
+// Have the function to find nearest Fibonacci numbers, that the parameter is array.
+
+// Example: 
+
+// ```jsx
+// arr = [15,1,3]
+// ```
+
+// then your program should made output = 2
+
+// Because by the above input example [15,1,3], if we add up them it will 15+1+3 = 19,
+//  and the nearest fibonacci of 19 is 21, so we need to **add 2 then it can be 21.
