d00dfcd704fba7791cf54827483fa21a23a24df9
Edwin Dharmawan <edwindharmawan@refactory.id>
2021-04-29 16:10:10 +0700

    intial commit

---
+++ Logic/palindrome/index.js
@@ -0,0 +1,7 @@
+// Radar                 # --> palindrome
+// Malam                 # --> palindrome
+// Kasur ini rusak       # --> palindrome
+// Ibu Ratna antar ubi   # --> palindrome
+// Malas                 # --> not palindrome
+// Makan nasi goreng     # --> not palindrome
+// Balonku ada lima      # --> not palindrome
